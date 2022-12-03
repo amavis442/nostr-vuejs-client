b12cb2d89f1c1bd03e03b53fe8d71c2524a4c8ea
Patrick Teunissen <p.teunissen18@gmail.com>
2022-12-03 03:05:01 +0100

    Clenaup maps

--- src/stores/events.ts
+++ src/stores/events.ts
@@ -12,15 +12,11 @@ export type Event = {
 
 export const useEventStore = defineStore("events", {
   state: () => {
-    const events: { [key: string]: Event } = {};
-    const map = new Map();
-    const map2 = new Map();
+    const events = new Map();
     const replyMap = new Map();
     const str = "";
     return {
       events,
-      map,
-      map2,
       replyMap,
       str,
     };
@@ -35,42 +31,31 @@ export const useEventStore = defineStore("events", {
         event.pubkey != "" &&
         event.created_at > 0
       ) {
-        this.events[event.id] = event;
-        this.map2.set(event.id, event);
-        this.map.set(event.created_at, event);
+        this.events.set(event.id, event);
       }
     },
     remove(id: string) {
-      this.map.delete(this.events[id].created_at);
-      this.map2.delete(id);
+      this.events.delete(id);
       this.replyMap.delete(id);
-      delete this.events[id];
     },
     addReply(id: string, replyid: string) {
-      this.map.set(id, replyid);
+      this.replyMap.set(id, replyid);
     },
     getReply(id: string) {
-      return this.map.get(id);
-    },
-    getFromMap(id: string): Event | undefined {
-      return this.map2.get(id);
-    },
-    getMap() {
-      return this.map2;
+      return this.replyMap.get(id);
     },
     all() {
       const sorted_map_by_str_values = new Map(
-        [...this.map2.entries()].sort((a, b) => {
+        [...this.events.entries()].sort((a, b) => {
           return a[1].created_at - b[1].created_at;
         })
       );
 
       return sorted_map_by_str_values;
-      //return new Map([...this.map.entries()].sort());
     },
     get(id: string): Event | null {
-      if (this.events[id]) {
-        return this.events[id];
+      if (this.events.get(id)) {
+        return this.events.get(id);
       }
       return null;
     },
