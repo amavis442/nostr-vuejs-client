37681e96c8c8a6cfa4964a3e9baf9a9c469cdf35
Patrick Teunissen <p.teunissen18@gmail.com>
2022-12-04 19:08:51 +0100

    Wait for all relays to finish

--- src/nostr/nostr.ts
+++ src/nostr/nostr.ts
@@ -4,12 +4,18 @@ import {
   type SubscriptionCallback,
 } from "nostr-tools";
 import { useUserStore } from "@/stores/users";
+import { useRelayStore } from "@/stores/relays";
 
 export const pool = relayPool();
 
 export async function sendRequest(filter: {}): Promise<Array<any>> {
   const channel = Math.random().toString().slice(2);
   const data: any = [];
+
+  const relayStore = useRelayStore();
+  const availableRelays = relayStore.relays.length;
+  const eoseRelays: string[] = [];
+
   return new Promise((resolve) => {
     const sub = pool.sub(
       {
@@ -17,9 +23,12 @@ export async function sendRequest(filter: {}): Promise<Array<any>> {
         filter: filter,
       },
       channel,
-      () => {
-        sub.unsub();
-        resolve(data);
+      (url: string) => {
+        eoseRelays.push(url);
+        if (eoseRelays.length == availableRelays) {
+          sub.unsub();
+          resolve(data);
+        }
       }
     );
   });
